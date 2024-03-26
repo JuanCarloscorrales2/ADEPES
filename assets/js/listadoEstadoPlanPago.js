a326d0cbb63fe655f67b4c5f531b0bd737da877b
SebastianSandres16 <117394594+SebastianSandres16@users.noreply.github.com>
2024-03-25 21:24:02 -0600

    Agrego el control de evento de EstadoPlanPago.

--- assets/js/listadoEstadoPlanPago.js
+++ assets/js/listadoEstadoPlanPago.js
@@ -21,3 +21,31 @@ function generarPDF() {
   $('#btn_descargar_reporteEstPlanPago_pdf').on('click', function() {
     generarPDF();
   });
+
+  //funcion para controlar el evento click fuera del modal
+function eventoCerrarModal(){
+
+  if($('#descripcion_estadoplanpago').val() != ""){ //validad que los input hayan datos
+      
+    Swal.fire({
+        title: '¿Estás seguro?',
+        text: "La información ingresada se perderá.",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Sí, salir',
+        cancelButtonText: 'Cancelar'
+      }).then((result) => {
+        // Si el usuario confirma, cierra el modal 
+        if (result.isConfirmed) {
+          //limpia los inputs
+          $('#descripcion_estadoplanpago').val('');
+    
+        } else {
+          // Si el usuario cancela, vuelve a abrir el modal
+          $('#registral_estadoplanpago').modal('show');
+        }
+      });
+  }
+}
