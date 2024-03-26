535cd1b4d4034a447cc560453b4615f3b0d9a9fe
SebastianSandres16 <117394594+SebastianSandres16@users.noreply.github.com>
2024-03-25 21:01:33 -0600

    Agrego el control de evento de Parentesco.

--- assets/js/listadoParentesco.js
+++ assets/js/listadoParentesco.js
@@ -21,3 +21,32 @@ function generarPDF() {
   $('#btn_descargar_rParentesco_pdf').on('click', function() {
     generarPDF();
   });
+
+  //funcion para controlar el evento click fuera del modal
+function eventoCerrarModal(){
+
+  if($('#descripcion_Parentesco').val() != ""){ //validad que los input hayan datos
+      
+    Swal.fire({
+        title: '¿Estás seguro?',
+        text: "La información ingresada se perderá.",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Sí, salir',
+        cancelButtonText: 'Cancelar'
+      }).then((result) => {
+        // Si el usuario confirma, cierra el modal 
+        if (result.isConfirmed) {
+          //limpia los inputs
+          $('#descripcion_Parentesco').val('');
+    
+        } else {
+          // Si el usuario cancela, vuelve a abrir el modal
+          $('#registral_Parentesco').modal('show');
+        }
+      });
+  }
+ 
+}
