386c08391cd376e095b7d897b319d8a3d6a1555f
JuanCarloscorrales2 <106850580+JuanCarloscorrales2@users.noreply.github.com>
2024-04-03 09:34:00 -0600

    Permisos nuevas tablas

--- pages/TBLObjetos.php
+++ pages/TBLObjetos.php
@@ -6,7 +6,7 @@ if (isset($_SESSION["user"])) {
 	require "../model/Permisos.php";
 	$permiso = new Permisos();
 	$rol = $_SESSION["user"]["idRol"];
-	$tiene_permiso = $permiso->ListarPermisosRol(11, $rol);
+	$tiene_permiso = $permiso->ListarPermisosRol(39, $rol);
 	$_SESSION["actualizar"] = $tiene_permiso ? $tiene_permiso["actualizar"] : 0;
 	$_SESSION["eliminar"] = $tiene_permiso ? $tiene_permiso["eliminar"] : 0;
 	$_SESSION["consultar"] = $tiene_permiso ? $tiene_permiso["consultar"] : 0;
