e38a88836ac61587065687e7c42332b82ce9cc38
SebastianSandres16 <117394594+SebastianSandres16@users.noreply.github.com>
2024-04-06 02:37:28 -0600

    Agg. ajuste de reporte en listado usuarios.

--- pages/fpdf/listadoUsuarios.php
+++ pages/fpdf/listadoUsuarios.php
@@ -74,6 +74,110 @@
                 $hoy = date('d/m/Y H:i:s');
                 $this->Cell(470, 10, utf8_decode('Fecha y Hora de impresión: '.$hoy), 0, 0, 'C');
             } // Fin de pie de pagina.
+
+            /******************************* METODO PARA AJUSTAR CELDAS ********************************** */
+   protected $widths;
+   protected $aligns;
+
+   function SetWidths($w)
+   {
+       // Set the array of column widths
+       $this->widths = $w;
+   }
+
+   function SetAligns($a)
+   {
+       // Set the array of column alignments
+       $this->aligns = $a;
+   }
+
+   function Row($data)
+   {
+       // Calculate the height of the row
+       $nb = 0;
+       for($i=0;$i<count($data);$i++)
+           $nb = max($nb,$this->NbLines($this->widths[$i],$data[$i]));
+       $h = 5*$nb;
+       // Issue a page break first if needed
+       $this->CheckPageBreak($h);
+       // Draw the cells of the row
+       for($i=0;$i<count($data);$i++)
+       {
+           $w = $this->widths[$i];
+           $a = isset($this->aligns[$i]) ? $this->aligns[$i] : 'L';
+           // Save the current position
+           $x = $this->GetX();
+           $y = $this->GetY();
+           // Draw the border
+           $this->Rect($x,$y,$w,$h);
+           // Print the text
+           $this->MultiCell($w,5,$data[$i],0,$a);
+           // Put the position to the right of the cell
+           $this->SetXY($x+$w,$y);
+       }
+       // Go to the next line
+       $this->Ln($h);
+   }
+
+   function CheckPageBreak($h)
+   {
+       // If the height h would cause an overflow, add a new page immediately
+       if($this->GetY()+$h>$this->PageBreakTrigger)
+           $this->AddPage($this->CurOrientation);
+   }
+
+   function NbLines($w, $txt)
+   {
+       // Compute the number of lines a MultiCell of width w will take
+       if(!isset($this->CurrentFont))
+           $this->Error('No font has been set');
+       $cw = $this->CurrentFont['cw'];
+       if($w==0)
+           $w = $this->w-$this->rMargin-$this->x;
+       $wmax = ($w-2*$this->cMargin)*1000/$this->FontSize;
+       $s = str_replace("\r",'',(string)$txt);
+       $nb = strlen($s);
+       if($nb>0 && $s[$nb-1]=="\n")
+           $nb--;
+       $sep = -1;
+       $i = 0;
+       $j = 0;
+       $l = 0;
+       $nl = 1;
+       while($i<$nb)
+       {
+           $c = $s[$i];
+           if($c=="\n")
+           {
+               $i++;
+               $sep = -1;
+               $j = $i;
+               $l = 0;
+               $nl++;
+               continue;
+           }
+           if($c==' ')
+               $sep = $i;
+           $l += $cw[$c];
+           if($l>$wmax)
+           {
+               if($sep==-1)
+               {
+                   if($i==$j)
+                       $i++;
+               }
+               else
+                   $i = $sep+1;
+               $sep = -1;
+               $j = $i;
+               $l = 0;
+               $nl++;
+           }
+           else
+               $i++;
+       }
+       return $nl;
+   } // FIN DEL METODO AJUSTAR CELDAS
         } //Fin de libreria de reporte.
         //Ahora instanciar para la pagina y el formato y su contenido.
         $pdf = new PDF();
@@ -83,17 +187,11 @@
         $i = 0;
         $pdf->SetFont('Arial', '', 10);
         $pdf->SetDrawColor(163, 163, 163); //colorBorde
-
+        $pdf->SetWidths(array(40, 50, 45, 25, 60, 42));    
         //construir el contenido del PDF DE LOS DATOS OBTENIDOS DEL DATATABLESS
         foreach ($datosFiltradosOrdenados as $fila) {
-            //$pdf->Cell(18, 10, $fila['id'], 1, 0, 'C', 0); //LOS NOMBRE SON DE LA FUNCION: LlenarTablaUsuario  JS
-            $pdf->Cell(40, 10, $fila['Usuario'], 1, 0, 'C', 0);
-            $pdf->Cell(50, 10, $fila['Nombre'], 1, 0, 'C', 0);
-            $pdf->Cell(45, 10, utf8_decode($fila['Rol']), 1, 0, 'C', 0);
-            $pdf->Cell(25, 10, $fila['Estado'], 1, 0, 'C', 0);
-            $pdf->Cell(60, 10, $fila['Correo'], 1, 0, 'J', 0);
-            $pdf->Cell(42, 10, $fila['Fecha'], 1, 1, 'C', 0);
-        //$pdf->Ln();
+            $pdf->Row(array(utf8_decode($fila['Usuario']), utf8_decode($fila['Nombre']), utf8_decode($fila['Rol']),
+            ($fila['Estado']), utf8_decode($fila['Correo']), utf8_decode($fila['Fecha'])), 0 );
         } //Fin de obtener todos los datos.
         // Enviar el PDF al navegador para su descarga 
         $pdf->Output('Reporte_de_usuarios.pdf', 'I'); //nombre de descarga
